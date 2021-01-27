22ec7925c433c67728c924998f46ed09751c5b72
Hasan Gökçe <kodyap@gmail.com>
2021-01-27 12:44:10 +0300

    #30 ObjectId() solved

--- routes/boards.js
+++ routes/boards.js
@@ -1,21 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
+const ObjectId = require('mongodb').ObjectId;
 /**
  * /boards
  */
 
 // Home page route.
 router.get('/', function (req, res) {
-  res.send('Boards route module home page');
+  req.app.db.collection('my_posts').find().toArray(function (err, result) {
+    if (err) throw err
+    res.send(result)
+})
 })
 
 // Single board route
+/**
+ * GET http://localhost:4001/boards/6006f7a257638c51a36a59c0
+ */
+
 router.get('/:id', (req, res) => {
-  req.app.db.collection('my_posts').find().toArray(function (err, result) {
-      if (err) throw err
-      res.send(result)
-  })
+  console.log(req.params.id)
+  const board_id = new ObjectId(req.params.id)
+  req.app.db.collection("my_posts").findOne({"_id": board_id }, function(err, result) {
+    // req.app.db.collection("my_posts").findOne({"_id":{$in: [board_id]} }, function(err, result) {
+    if (err) throw err;
+    if(result){
+      res.status(200).send(result)
+    } else {
+      res.status(204).send({'message' : "Not found"})
+    }
+  });
 })
 
 
--- routes/home.js
+++ routes/home.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/**
- * /
- */
-
 // Home page route.
 router.get('/', function (req, res) {
   res.send('Boards route module home page');
 })
-
-
 
 module.exports = router;
